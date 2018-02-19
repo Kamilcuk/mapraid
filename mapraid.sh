f94b74da73428cb531d5ae78f1c7ab0eba11ba9d
Kamil Cukrowski <kamilcukrowski@gmail.com>
2018-02-19 13:55:16 +0100

    Add restore function to mapraid

--- mapraid.sh
+++ mapraid.sh
@@ -10,17 +10,43 @@ usage() {
 	cat <<EOF
 Usage: 
 	$0 [OPTIONS] <mark|map> file [blocksize:=512]
+	$0 [OPTIONS] restore mapfile output [blocksize:=512]
 
 Options:
 	-h --help     - print this help and exit
 	-p            - print progressbar
 	-v --verbose  - increase verbose level
-	-i            - don't pring ignored blocks when mapping
+	-i            - pring when ignoring blocks when mapping
+	-f --force    - don't ask for confirmation on deleting files
+
+Modes:
+	mark    - writes blocksize chunks to file containing:
+	          <version string> <chunk number> <string of '0' characters> <md5sum of leading characters>\n
+	map     - reads blocksize chunks from file.
+			  Checks if chunks is valid and then extracts <chunk number> from each chunk.
+			  Prints out mapping information as
+			    <filename> <chunk number in filename> <extracted chunks number>
+	restore - reads mapfile in format as outputted by map
+			  copies block by block data from input files (contained in mapfile) to output
+
+Usage example:
+	\$ # create raid device from some loop files
+	\$ mdadm --create .... /dev/md100 /dev/loop1 /dev/loop2 /dev/loop3
+	\$ # mark sectors on raid
+	\$ $0 -p mark /dev/md100
+	\$ mdam --stop /dev/md100
+	\$ # map marked sectors to mapfile
+	\$ $0 -p map /dev/loop1 > mapfile
+	\$ $0 -p map /dev/loop2 >> mapfile
+	\$ $0 -p map /dev/loop3 >> mapfile
+	\$ # then you can restore raid content from /dev/loop{1,2,3} with
+	\$ $0 -p restore mapfile outputdevice
 
 Written by Kamil Cukrowski.
 Licensed jointly under MIT License and Beerware License.
 EOF
 	if [ -n "${1:-}" ]; then
+		echo
 		echo "ERROR:" "$@" >&2
 		exit 1
 	fi
@@ -42,8 +68,8 @@ progressbar() {
 	if [ "$notlen" -eq 0 ]; then post=""; else
 		post=$(printf ' %.0s' $(seq $notlen))
 	fi
-	echo -ne "[${pre}${post}] (${val}/${max})"$'\r'
-	if [ "$val" -eq "$max" ]; then echo; fi
+	echo -ne "[${pre}${post}] (${val}/${max})"$'\r' >&2
+	if [ "$val" -eq "$max" ]; then echo >&2; fi
 }
 
 assert() {
@@ -58,7 +84,7 @@ verbose() {
 	local -g VERBOSE_LVL
 	if [ "${VERBOSE_LVL:-0}" -gt "$1" ]; then
 		shift
-		echo "$@"
+		echo "$@" >&2
 	fi
 }
 
@@ -89,10 +115,32 @@ data_create() {
 	verbose 2 "Write '$VERSION $num <#${#fill} 0s> $md5sum' to file '$out', length=$outlen"
 }
 
+ignoreprint() {
+	local -g IGNOREPRINT
+	if ! ${IGNOREPRINT:-false}; then return; fi
+	local cnt
+	cnt=$1
+	shift
+	echo "# ignoring block $cnt:" "$@" >&2
+}
+
+confirm() {
+	local -g FORCE
+	if ${FORCE:-false}; then return; fi
+	local file tmp
+	file=$1
+	echo "This operation will erase all data on file '$file'."
+	echo "Are you sure you want to continue?"
+	read -p 'Answer [y|n]: ' tmp
+	case "$tmp" in Y*|y*) ;; *) exit; ;; esac
+}
+
 mode:mark() {
 	local -g DEV DEVBCNT BS
 	local -i cnt 
 	local temp
+
+	confirm "$DEV"
 
 	temp=$(mktemp)
 	trap 'echo "INTERNAL ERROR: last command returned $?."; rm "$temp";' EXIT
@@ -113,15 +161,6 @@ mode:mark() {
 	assert [ "$cnt" -eq "$DEVBCNT" ] "Internal error: cnt=$cnt != devbcnt=$DEVBCNT"
 
 	verbose 0 "Done"
-}
-
-ignoreprint() {
-	local -g IGNOREPRINT
-	if ! ${IGNOREPRINT:-true}; then return; fi
-	local cnt
-	cnt=$1
-	shift
-	echo "ignoring block $cnt:" "$@"
 }
 
 mode:map() {
@@ -164,12 +203,11 @@ mode:map() {
 			((++ignored)); ((++cnt)); continue;
 		fi
 
-		echo "$cnt -> $readcnt"
+		echo "$DEV $cnt $readcnt"
 
 		((++cnt))
 	done
 
-	progressbar "$cnt" "$DEVBCNT"
 	rm "$temp" "$temp2"
 	trap '' EXIT
 	assert [ "$cnt" -eq "$DEVBCNT" ] "Internal read error: cnt=$cnt != devbcnt=$DEVBCNT"
@@ -177,37 +215,103 @@ mode:map() {
 	verbose 0 "Done. IgnoredBlocksCnt=$ignored"
 }
 
+mode:restore() {
+	local -g BS
+	local -i cnt mapfilelen outfilelen_shouldbe
+	local mapfile outfile tmp i tmp2 infiles tmp3
+	mapfile=$1
+	outfile=$2
+	BS=${3:-512}
+
+	tmp=$(cut -d' ' -f1,2 "$mapfile" | uniq -d)
+	assert [ -z "$tmp" ] "mapfile error:: duplicate in first and second column"
+	tmp=$(cut -d' ' -f2 "$mapfile")
+	for i in $tmp; do assert [ "$i" -eq "$i" ] "mapfile error: Value in second column is not a number: $i"; done
+	tmp=$(cut -d' ' -f3 "$mapfile")
+	for i in $tmp; do assert [ "$i" -eq "$i" ] "mapfile error: Value in third column is not a number: $i"; done
+	tmp=$(uniq -d <<<"$tmp")
+	assert [ -z "$tmp" ] "mapfile error: duplicate found in third column"
+	
+	infiles=$(cut -d' ' -f1 "$mapfile" | uniq)
+	for i in $infiles; do assert [ -e "$i" ] "mapfile error: mentioned file '$i' that does not exists!"; done
+
+	mapfilelen=$(wc -l "$mapfile" | cut -d' ' -f1)
+	outfilelen_shouldbe=$((BS*mapfilelen))
+
+	verbose 0 "Restoring $outfile of size blocksize=$BS*$mapfilelen from files:" $infiles
+
+	confirm "$outfile"
+
+	cnt=0
+	while IFS=' ' read -r infile inblock outblock; do
+		assert [ "$inblock" -eq "$inblock" ] "Error inblock=$inblock is not a number"
+		assert [ "$outblock" -eq "$outblock" ] "Error outblock=$outblock is not a number"
+		progressbar "$cnt" "$mapfilelen"
+		verbose 1 "Coyping if=$infile skip=$inblock of=$outfile seek=$outblock"
+		dd if="$infile" skip="$inblock" of="$outfile" seek="$outblock" bs="$BS" count=1 status=none
+		((++cnt))
+	done < <(sort -t' ' -n -k3 "$mapfile")
+
+	progressbar "$cnt" "$mapfilelen"
+
+	tmp=$(wc -c "$outfile" | cut -d' ' -f1)
+	tmp2=$((mapfilelen*BS))
+	assert [ "$tmp" -eq "$tmp2" ] "outputfile has different size then expected ! outfilelen=$tmp -eq (mapfilelen=$mapfilelen*BS=$BS)=$tmp2"
+
+	verbose 0 "Success"
+}
+
 # Main ################################
 
-OPTS=$(getopt -n "mapraid.sh" -o hpvi -l help,verbose -- "$@")
+tmp="${BASH_VERSION//.*}" 
+if [ "$tmp" -lt 4 ]; then
+	echo "ERROR: this scripts needs bash version at least 4" >&2
+	exit 255
+fi
+
+hash bash >/dev/null 2>/dev/null
+for i in getopt cmp dd md5sum; do
+	if ! hash "$i" >/dev/null 2>/dev/null; then
+		echo "ERROR: command '$i' was not found in PATH" >&2
+		exit 255
+	fi
+done
+
+OPTS=$(getopt -n "mapraid.sh" -o hpvif -l help,verbose,force -- "$@")
 eval set -- "$OPTS"
 while (($#)); do
 	case "$1" in
 		-h|--help) usage; exit 0; ;;
 		-p) : ${PROGRESSBAR:=true}; ;;
 		-v|--verbose) : ${VERBOSE_LVL:=0}; ((++VERBOSE_LVL)); ;;
-		-i) : ${IGNOREPRINT:=false}; ;;
+		-i) : ${IGNOREPRINT:=true}; ;;
+		-f|--force) : ${FORCE:=true}; ;;
 		--|*) shift; break; ;;
 	esac
 	shift
 done
 
-if [ $# -ne 3 ]; then usage "Wrong number of arguments: $#:" "$@"; fi;
+if [ $# -lt 1 ]; then usage "Wrong number of arguments: $#:" "$@"; fi;
 
 MODE=${1:-}
+shift
 case "$MODE" in
-	mark|map) ;;
+	mark|map) 
+		if [ $# -ne 1 -a $# -ne 2 ]; then usage "Wrong number of arguments for mode $MODE: $#:" "$@"; fi;
+		DEV=$1
+		BS=${2:-512}
+		if [ ! -e "$DEV" ]; then usage "File '$dev' does not exists"; fi;
+		devsize=$(wc -c "$DEV" | cut -d' ' -f1)
+		DEVBCNT=$((devsize/BS))
+		assert [ $((devsize%BS)) -eq 0 ] "Size of '$DEV'=$devsize is not a multiplicity of blosize=$BS"
+
+		verbose 1 "MODE=$MODE DEV=$DEV BS=$BS devsize=$devsize DEVBCNT=$DEVBCNT"
+
+		"mode:${MODE}"
+		;;
+	restore)
+		if [ $# -ne 2 -a $# -ne 3 ]; then usage "Wrong number of arguments for mode $MODE: $#:" "$@"; fi;
+		mode:restore "$@"
+		;;
 	*) usage "Unknown mode $mode"; ;;
 esac
-shift
-DEV=$1
-shift
-BS=${1:-512}
-
-devsize=$(wc -c "$DEV" | cut -d' ' -f1)
-DEVBCNT=$((devsize/BS))
-assert [ $((devsize%BS)) -eq 0 ] "Size of '$DEV'=$devsize is not a multiplicity of blosize=$BS"
-
-verbose 1 "MODE=$MODE DEV=$DEV BS=$BS devsize=$devsize DEVBCNT=$DEVBCNT"
-
-"mode:${MODE}"
--- mapraid_test.sh
+++ mapraid_test.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 set -euo pipefail
+set -x
 
 assert() {
 	if ! "${@:1:$(($#-1))}"; then 
-		echo "${@:$#}" >&2;
+		echo "Assertion '${@:1:$(($#-1))}' failed in ${FUNCNAME[-1]}:" >&2
+		echo " ${@:$#}" >&2;
 		exit 1;
 	fi
 }			
 
+mapraid=$(readlink -f ./mapraid.sh)
+mapraid() {
+	"$mapraid" "$@"
+}
+
 test1() {
-	"$mapraid" mark image 128
-	out=$("$mapraid" map image 128)
-	shouldbe=$(for i in $(seq 0 9); do echo "$i -> $i"; done)
+	mapraid mark -f image 128
+	out=$(mapraid map image 128)
+	shouldbe=$(for i in $(seq 0 9); do echo "image $i $i"; done)
 	assert [ "$out" = "$shouldbe" ] "test1 error $out"
+	echo "$out" > mapfile
+	mapraid restore -f mapfile image_restored 128
+	assert cmp image image_restored "test1 error restoring file"
 	echo "test1 ok"
 }
 
 test2() {
 	dd if=image of=image2 count=5 bs=128 skip=5 status=none
 	dd if=image of=image2 count=5 bs=128 seek=5 status=none
-	out=$("$mapraid" map image2 128)
-	shouldbe=$(cnt=0; for i in $(seq 5 9) $(seq 0 4); do echo "$cnt -> $i"; ((cnt++)); done)
+	out=$(mapraid map image2 128)
+	shouldbe=$(cnt=0; for i in $(seq 5 9) $(seq 0 4); do echo "image2 $cnt $i"; ((cnt++)); done)
 	assert [ "$out" = "$shouldbe" ] "test2 error"	
+	echo "$out" > mapfile
+	mapraid restore -f mapfile image_restored 128
+	assert cmp image image_restored "test2 error restoring file"
 	echo test2 ok
 }
 test3() {
@@ -49,19 +62,22 @@ test3() {
 	sudo chown $UID $md
 	sync
 
-	"$mapraid" mark $md $chunk
-	"$mapraid" map $md $chunk
+	"$mapraid" mark -f $md $chunk
+	cat $md > disc
 
 	sudo mdadm --stop $md
 	sudo losetup -d ${loop}{1,2,3}
 	trap '' EXIT
 
-	"$mapraid" map disc1 $chunk
-	"$mapraid" map disc2 $chunk
-	"$mapraid" map disc3 $chunk
+	"$mapraid" map disc1 $chunk >mapfile
+	"$mapraid" map disc2 $chunk >>mapfile
+	"$mapraid" map disc3 $chunk >>mapfile
+
+	"$mapraid" restore -f mapfile disc_restored $chunk
+	assert cmp disc disc_restored "test3 error restoring file"
+
 }
 
-mapraid=$(readlink -f ./mapraid.sh)
 mkdir -p /tmp/10
 cd /tmp/10
 truncate image --size $((128*10))
